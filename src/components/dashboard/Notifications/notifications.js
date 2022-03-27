049b1c56293c9468e340eca1c50c9db7e35f33cb
AdityaConnect18 <adityaconnect18@gmail.com>
2022-03-27 16:15:03 +0530

    After Bar Added

--- src/components/dashboard/Notifications/notifications.js
+++ src/components/dashboard/Notifications/notifications.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NotificationCard from './notificationCard';
 import classes from './notifications.module.css';
+import Bar from '../Bar/bar';
 
 const Notifications = () => {
     return(
         <div className={classes.MainContainer}>
             <div className={classes.Heading}>Notifications</div>
-            <NotificationCard/>
+            {/* <NotificationCard/> */}
+            <Bar/>  
         </div>
     );
 }
