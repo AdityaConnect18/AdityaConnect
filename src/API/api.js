3056c5fee5b8fe8785152979063d43f4ec04a013
charankurru <61787496+charankurru@users.noreply.github.com>
2022-06-06 12:56:23 +0530

    direct api connect

--- src/API/api.js
+++ src/API/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API = process.env.REACT_APP_BACKEND_API_KEY;
+// const API = process.env.REACT_APP_BACKEND_API_KEY
+const API = "http://Adityaconnectbackend-env.eba-v5evckwp.ap-south-1.elasticbeanstalk.com"
 const api = axios.create({
   baseURL: `${API}`,
   headers: {
